f8c37e8d733a16f04c4b3b1662127846f6cee36d
FransK <kouwenhoven.frans@gmail.com>
2023-02-21 09:45:36 -0800

    Change create to use NicsGameService

--- dev/src/playground/apis.py
+++ dev/src/playground/apis.py
@@ -8,17 +8,14 @@ from cardgame.apis import CardgameAPI
 from .serializers import GameSerializer
 from .services.nics_game_service import NicsGameService
 
-game_service = NicsGameService()
-
 
 @api_view(['GET', 'POST'])
 def game_list(request):
     if request.method == 'GET':
-        serializer = GameSerializer(game_service._games, many=True)
+        serializer = GameSerializer(NicsGameService()._games, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = GameSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        game_service._games.append(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
--- dev/src/playground/serializers/game_serializer.py
+++ dev/src/playground/serializers/game_serializer.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 
 from ..models.game_model import Game
+from ..services.nics_game_service import NicsGameService
 
 
 class GameSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.IntegerField(required=False)
     name = serializers.CharField(max_length=255)
     round_number = serializers.IntegerField(required=False)
     started_at = serializers.DateTimeField(required=False)
     status = serializers.CharField(max_length=1, required=False)
 
     def create(self, validated_data):
-        return Game(**validated_data)
+        name = validated_data['name']
+        return NicsGameService().create_game(name)
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
