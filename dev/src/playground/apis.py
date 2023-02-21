9656495d383ddc18704ec12648b6d227d086752f
FransK <kouwenhoven.frans@gmail.com>
2023-02-21 15:41:42 -0800

    fix game getter

--- dev/src/playground/apis.py
+++ dev/src/playground/apis.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import GameSerializer
@@ -7,3 +8,16 @@ from .services.nics_game_service import NicsGameService
 class GameViewSet(ModelViewSet):
     queryset = NicsGameService()._games
     serializer_class = GameSerializer
+    lookup_field = 'id'
+
+    def get_object(self):
+        obj = None
+        for game in NicsGameService()._games:
+            if game.id == int(self.kwargs['id']):
+                obj = game
+                break
+
+        if not obj:
+            raise Http404
+
+        return obj
