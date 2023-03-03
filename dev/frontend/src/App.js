1a3ca3efa59dfce052773c18e5c83ea0f7890dec
FransK <kouwenhoven.frans@gmail.com>
2023-03-02 18:10:02 -0800

    fix resizing, refactor click handlers up

--- dev/frontend/src/App.js
+++ dev/frontend/src/App.js
@@ -1,26 +1,12 @@
 import { useState } from "react";
 
-const handCards = [
-  { number: 3, point_value: 1 },
-  { number: 11, point_value: 5 },
-  { number: 55, point_value: 7 }
-];
-
-const pileCards = [
-  { number: 8, point_value: 1 },
-  { number: 35, point_value: 2 },
-  { number: 40, point_value: 3 },
-  { number: 77, point_value: 5 },
-  { number: 89, point_value: 1 },
-];
-
 function Instructions() {
   return (
     <div className="instructions">
       <span>Instructions:</span>
-      <text className="instructions-text">
+      <div className="instructions-text">
         This is the instructions text.
-      </text>
+      </div>
     </div>
   )
 }
@@ -29,154 +15,237 @@ function Score() {
   return (
     <div className="score">
       <span>Score:</span>
-      <text className="score-text">
+      <div className="score-text">
         This is the score text.
-      </text>
+      </div>
     </div>
   )
 }
 
-function Card({ number, value }) {
-  return <button className="card"><div>{number}</div><div>{value}</div></button>
+function Card({ number, value, selected, onCardClick }) {
+  return (
+    <button className="card" onClick={onCardClick}>
+      <div>{number}</div>
+      <div>{value}</div>
+      <div>{selected.toString()}</div>
+    </button>
+  );
 }
 
 function BullheadStack() {
-  return <Card number="Bull" value="Head" />;
+  function handleClick() {
+    console.log('bullhead clicked');
+  }
+  return <Card number="Bull" value="Head" selected={false} onCardClick={handleClick} />;
 }
 
-function Hand() {
-  const handItems = handCards.map(card =>
-    <Card key={card.number} number={card.number} value={card.point_value} />
+function Hand({ cards, selectedCard, onCardClick }) {
+  const handItems = cards.map(card =>
+    <Card key={card.number}
+      number={card.number}
+      value={card.pointValue}
+      selected={card.number === selectedCard}
+      onCardClick={() => onCardClick(card.number)} />
   );
   return (
     <div className="player-hand">{handItems}</div>
   );
 }
 
-function Pile() {
-  const pileItems = pileCards.map(card =>
-    <Card key={card.number} number={card.number} value={card.point_value} />
+function Pile({ number, cards, onCardClick }) {
+  const pileCards = cards.map(card =>
+    <Card key={card.number} number={card.number} value={card.pointValue} selected={false} onCardClick={() => onCardClick(card.number)} />
   );
+
   return (
     <div className="table-pile">
-      <div className="pile-label">Pile 1:</div>
-      <div className="pile-items">{pileItems}</div>
+      <div className="pile-label">Pile {number}:</div>
+      <div className="pile-items">{pileCards}</div>
     </div>
   );
 }
 
-function Player() {
+function Player({ playerNumber, handCards }) {
+  const [cardSelected, setCardSelected] = useState(-1);
+
+  function handleClick(number) {
+    if (cardSelected === number) {
+      setCardSelected(-1);
+      console.log('clicked ' + number.toString());
+    }
+    else {
+      setCardSelected(number);
+      console.log('clicked ' + number.toString());
+    }
+  }
+
   return (
     <div className="player">
       <div>
         <BullheadStack />
       </div>
       <div>
-        <Hand />
-        Player 1 Name
+        <Hand cards={handCards} selectedCard={cardSelected} onCardClick={handleClick} />
+        Player {playerNumber} Name
       </div>
     </div>
   );
 }
 
-function Table() {
+function Table({ pileCards, onPileClicked }) {
+  let table = [];
+  for (let i = 0; i < 4; i++) {
+    table.push(<Pile key={i + 1} number={i + 1} cards={pileCards[i]} onCardClick={() => onPileClicked(i + 1)} />)
+  }
   return (
     <div className="table">
-      <Pile />
-      <Pile />
-      <Pile />
-      <Pile />
+      {table}
     </div>
   );
 }
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
+function Cardgame() {
+  const handCards1 = [
+    { number: 3, pointValue: 1 },
+    { number: 11, pointValue: 5 },
+    { number: 55, pointValue: 7 }
+  ];
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
-    if (squares[i] || calculateWinner(squares)) {
-      return;
-    }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = "X";
-    }
-    else {
-      nextSquares[i] = "O";
-    }
-    onPlay(nextSquares);
-  }
+  const handCards2 = [
+    { number: 1, pointValue: 1 },
+    { number: 22, pointValue: 5 },
+    { number: 100, pointValue: 3 }
+  ];
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  }
-  else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
+  const pileCards = [[
+    { number: 8, pointValue: 1 },
+    { number: 35, pointValue: 2 },
+    { number: 40, pointValue: 3 },
+    { number: 77, pointValue: 5 },
+    { number: 89, pointValue: 1 },
+  ],
+  [
+    { number: 9, pointValue: 1 },
+    { number: 25, pointValue: 2 },
+    { number: 50, pointValue: 3 },
+    { number: 66, pointValue: 5 },
+    { number: 98, pointValue: 1 },
+  ],
+  [
+    { number: 2, pointValue: 1 },
+    { number: 15, pointValue: 2 },
+    { number: 20, pointValue: 3 },
+    { number: 33, pointValue: 5 },
+    { number: 46, pointValue: 1 },
+  ],
+  [
+    { number: 4, pointValue: 1 },
+    { number: 5, pointValue: 2 },
+    { number: 60, pointValue: 3 },
+    { number: 88, pointValue: 5 },
+    { number: 93, pointValue: 1 },
+  ]];
 
-  let board = []
-  for (let i = 0; i < 3; i++) {
-    let row = []
-    for (let j = 0; j < 3; j++) {
-      let k = i * 3 + j;
-      row.push(<Square key={k} value={squares[k]} onSquareClick={() => handleClick(k)} />);
-    }
-    board.push(<div key={i} className="board-row">{row}</div>);
+  function handlePileClicked(pileNumber) {
+    console.log('Pile clicked ' + pileNumber);
   }
 
   return (
     <>
-      <div className="status">{status}</div>
-      {board}
+      <Player key={1} playerNumber={1} handCards={handCards1} />
+      <Table pileCards={pileCards} onPileClicked={handlePileClicked} />
+      <Player key={2} playerNumber={2} handCards={handCards2} />
     </>
   );
 }
 
+// function Square({ value, onSquareClick }) {
+//   return (
+//     <button className="square" onClick={onSquareClick}>
+//       {value}
+//     </button>
+//   );
+// }
+
+// function Board({ xIsNext, squares, onPlay }) {
+//   function handleClick(i) {
+//     if (squares[i] || calculateWinner(squares)) {
+//       return;
+//     }
+//     const nextSquares = squares.slice();
+//     if (xIsNext) {
+//       nextSquares[i] = "X";
+//     }
+//     else {
+//       nextSquares[i] = "O";
+//     }
+//     onPlay(nextSquares);
+//   }
+
+//   const winner = calculateWinner(squares);
+//   let status;
+//   if (winner) {
+//     status = "Winner: " + winner;
+//   }
+//   else {
+//     status = "Next player: " + (xIsNext ? "X" : "O");
+//   }
+
+//   let board = []
+//   for (let i = 0; i < 3; i++) {
+//     let row = []
+//     for (let j = 0; j < 3; j++) {
+//       let k = i * 3 + j;
+//       row.push(<Square key={k} value={squares[k]} onSquareClick={() => handleClick(k)} />);
+//     }
+//     board.push(<div key={i} className="board-row">{row}</div>);
+//   }
+
+//   return (
+//     <>
+//       <div className="status">{status}</div>
+//       {board}
+//     </>
+//   );
+// }
+
 export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
+  // const [history, setHistory] = useState([Array(9).fill(null)]);
+  // const [currentMove, setCurrentMove] = useState(0);
+  // const xIsNext = currentMove % 2 === 0;
+  // const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory);
-    setCurrentMove(nextHistory.length - 1);
-  }
+  // function handlePlay(nextSquares) {
+  //   const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+  //   setHistory(nextHistory);
+  //   setCurrentMove(nextHistory.length - 1);
+  // }
 
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
+  // function jumpTo(nextMove) {
+  //   setCurrentMove(nextMove);
+  // }
 
-  const moves = history.map((squares, move) => {
-    if (move === currentMove) {
-      return (
-        <li key={move}>
-          You are at move #{move}
-        </li>
-      )
-    }
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    }
-    else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
-  });
+  // const moves = history.map((squares, move) => {
+  //   if (move === currentMove) {
+  //     return (
+  //       <li key={move}>
+  //         You are at move #{move}
+  //       </li>
+  //     )
+  //   }
+  //   let description;
+  //   if (move > 0) {
+  //     description = 'Go to move #' + move;
+  //   }
+  //   else {
+  //     description = 'Go to game start';
+  //   }
+  //   return (
+  //     <li key={move}>
+  //       <button onClick={() => jumpTo(move)}>{description}</button>
+  //     </li>
+  //   );
+  // });
 
   return (
     <>
@@ -185,9 +254,7 @@ export default function Game() {
           <Instructions />
         </div>
         <div className="cardgame-centre">
-          <Player />
-          <Table />
-          <Player />
+          <Cardgame />
         </div>
         <div className="cardgame-right">
           <Score />
@@ -205,22 +272,22 @@ export default function Game() {
   )
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+// function calculateWinner(squares) {
+//   const lines = [
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [0, 4, 8],
+//     [2, 4, 6]
+//   ];
+//   for (let i = 0; i < lines.length; i++) {
+//     const [a, b, c] = lines[i];
+//     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//       return squares[a];
+//     }
+//   }
+//   return null;
+// }
